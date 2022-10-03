adcc3fd479e30b3f34d68499e2001453ca600c9c
ucho456 <watapqxyz@gmail.com>
2022-10-03 17:15:09 +0900

    created TasksModule

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './task.entity';
+import { TasksController } from './tasks.controller';
+import { TasksService } from './tasks.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Task])],
-  providers: [],
-  controllers: [],
+  providers: [TasksService],
+  controllers: [TasksController],
 })
 export class TasksModule {}
